e2922cce59406b07e24c29cc39d313759848ac8b
Deepak Gautam <38098415+styloxstar@users.noreply.github.com>
2023-06-13 15:23:37 +0530

    added some chnages and served some static files

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const fs = require('fs')
+
 const port = 3000
+
+app.use('/',express.static(path.resolve(__dirname+"/static")))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(__dirname+'/static/home.html')
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
